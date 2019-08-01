2d44db4c9dd6553aa9803626220849ac584086a4
thistle <bcmertz@email.wm.edu>
2019-08-01 16:42:49 -0400

    add markdown

--- init.el
+++ init.el
@@ -46,3 +46,27 @@
 
 ;; Return to normal gc value
 (setq gc-cons-threshold 800000)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes nil)
+ '(display-line-numbers nil)
+ '(file-name-shadow-mode nil)
+ '(inhibit-startup-screen t)
+ '(markdown-command "/usr/local/bin/pandoc")
+ '(package-selected-packages
+   '(flymd exwm ace-window try use-package which-key counsel swiper company-go company flymake-go popup-complete evil neotree direx peep-dired ranger avy nlinum-relative nlinum ag xref-js2 js2-refactor js2-mode go-rename magit markdown-mode go-eldoc go-autocomplete auto-complete flycheck go-mode))
+ '(speedbar-show-unknown-files t)
+ '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ languages/custom-markdown.el
@@ -0,0 +1,5 @@
+;;;;;;;;;;;;;;;;;;;;;;; Markdown Config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package flymd
+  :ensure t
+  :bind ("C-c m" . flymd-flyit)) ; previews
