eafae671c651f5443c0a47f624e2b492e714a9e1
thistle <bcmertz@email.wm.edu>
2019-08-17 21:06:33 -0400

    Update company mode

--- languages/custom-golang.el
+++ languages/custom-golang.el
@@ -1,18 +1,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;; GOLANG CONFIGURATION;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; autocompletion with company mode
-(require 'company)
-(setq company-tooltip-limit 20)                      ; bigger popup window
-(setq company-idle-delay .5)                         ; decrease delay before autocompletion popup shows
-(setq company-echo-delay 0)                          ; remove annoying blinking
-(setq company-begin-commands '(self-insert-command)) ; start autocompletion only after typing
-(require 'yasnippet)
-(add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-*")
-
-;; TODO figure out ac-update-greedy (?) error, swap for company mode?
-;;(load ""go-autocomplete.el"")
-;;(require 'auto-complete-config)
-;;(ac-config-default)
 ;; (add-to-list 'load-path "~/go/src/github.com/dominikh//") only if not from melpa
 ;; (autoload 'go-mode "go-mode" nil t)
 ;; (add-to-list 'auto-mode-alist '("\\.go\\'" . go-mode))
@@ -36,6 +23,18 @@
  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/go-mode")   ; enable snippets
  (yas-global-mode 1)
 
+ ;; autocompletion with company mode
+ (use-package company
+   :defer t
+   :init
+   (progn
+     (setq company-tooltip-limit 20)                       ; bigger popup window
+     (setq company-idle-delay nil)                         ; don't autocomplete on typing, backtab instead
+     ;; (setq company-idle-delay .3)  			   ; decrease delay before autocompletion popup shows
+     (setq company-echo-delay 0)                           ; remove annoying blinking
+     ))
+;; (setq company-begin-commands '(self-insert-command))  ; start autocompletion after typing, if we want to ignore our special tab key we bind below
+
  (require 'company-go)
  (set (make-local-variable 'company-backends) '(company-go))
  (company-mode)
@@ -43,3 +42,7 @@
  (local-set-key (kbd "C-c C-r") 'go-rename))                     ; provide go-rename conveniently
 
 (add-hook 'go-mode-hook 'go-mode-setup)
+;; figure out ac-update-greedy (?) error, swap for company mode?
+;;(load ""go-autocomplete.el"")
+;;(require 'auto-complete-config)
+;;(ac-config-default)
