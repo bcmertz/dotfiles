4ae6f603ff17bbe03d2e00390f2ad2a4a5106197
thistle <bcmertz@email.wm.edu>
2019-09-24 15:26:15 -0400

    increase opacity

--- build_configs/st/config.h
+++ build_configs/st/config.h
@@ -86,7 +86,7 @@ unsigned int tabspaces = 8;
 
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
