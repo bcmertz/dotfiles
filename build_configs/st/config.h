af29e38c092f833abac3649faaedd1f0006b0102
thistle <bcmertz@email.wm.edu>
2019-09-25 01:23:45 -0400

    update opacity

--- build_configs/st/config.h
+++ build_configs/st/config.h
@@ -85,7 +85,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.88;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
