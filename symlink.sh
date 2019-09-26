477cf2fc84d28aff21b3af9f57e2e07534ed6ae3
thistle <bcmertz@email.wm.edu>
2019-09-25 23:21:11 -0400

    .profile symlink

--- symlink.sh
+++ symlink.sh
@@ -10,6 +10,7 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_aliases ~/.bash_aliases
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.coderc ~/.coderc
+ln -s ~/dotfiles/.profile ~/.profile
 
 # applications
 for f in $(ls -d ~/dotfiles/.local/share/applications/*); do ln -s $f ~/.local/share/applications; done
@@ -23,7 +24,6 @@ ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global
 
 # install custom scripts
-mkdir -p ~/.time # for time on computer cronjob
 for f in $(ls -d ~/dotfiles/.local/bin/*); do ln -s $f ~/.local/bin; done
 for f in $(ls -d ~/dotfiles/.local/bin/cron/*); do ln -s $f ~/.local/bin/cron; done
 # x
@@ -31,3 +31,6 @@ ln -s ~/dotfiles/.xinitrc ~/.xinitrc
 
 # lantern
 ln -s ~/dotfiles/.lanternrc ~/.lanternrc || echo ".lanternrc doesn't exist"
+
+
+# build_configs [TODO]
