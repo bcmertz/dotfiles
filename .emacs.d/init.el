d4f3ac7174800423f4037188b0082ec5e1be85bc
thistle <bcmertz@email.wm.edu>
2020-02-13 01:58:20 -0500

    themeing

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -22,6 +22,7 @@
 (load "general.el")                     ; my configuration
 (load "custom.el")                      ; auto-generated config
 (load "styling.el")                     ; styling config
+(add-hook 'after-init-hook (lambda () (load-theme 'monokai t)))
 
 ;;(require 'custom-exwm-config)           ; use exwm instead of default xinitrc
 ;;(custom-exwm-config)
@@ -44,29 +45,3 @@
 
 ;; Return to normal gc value
 (setq gc-cons-threshold 800000)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
- '(custom-enabled-themes nil)
- '(display-line-numbers nil)
- '(file-name-shadow-mode nil)
- '(gofmt-command "goimports" t)
- '(inhibit-startup-screen t)
- '(markdown-command "/usr/local/bin/pandoc")
- '(package-selected-packages
-   (quote
-    (tramp-term ssh flymd exwm ace-window try use-package which-key counsel swiper company-go company flymake-go popup-complete evil neotree direx peep-dired ranger avy nlinum-relative nlinum ag xref-js2 js2-refactor js2-mode go-rename magit markdown-mode go-eldoc go-autocomplete auto-complete flycheck go-mode)))
- '(speedbar-show-unknown-files t)
- '(winner-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- .emacs.d/languages/custom-js.el
+++ .emacs.d/languages/custom-js.el
@@ -1,25 +1,3 @@
 ;;;;;;;;;;;;;;;;;;;;;JS Configuration;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(require 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-;; Better imenu
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
-
-(require 'js2-refactor)
-(require 'xref-js2)
-
-(add-hook 'js2-mode-hook #'js2-refactor-mode)
-(js2r-add-keybindings-with-prefix "C-c C-r")
-(define-key js2-mode-map (kbd "C-k") #'js2r-kill)
-
-;; js-mode (which js2 is based on) binds "M-." which conflicts with xref, so
-;; unbind it.
-(define-key js-mode-map (kbd "M-.") nil)
-
-(add-hook 'js2-mode-hook (lambda ()
-  (add-hook 'xref-backend-functions #'xref-js2-xref-backend nil t)))
-
-(setq js2-mode-show-parse-errors t)
-(setq js2-mode-show-strict-warnings nil)
-(setq js2-strict-missing-semi-warning nil)
+(setq js-indent-level 2)
