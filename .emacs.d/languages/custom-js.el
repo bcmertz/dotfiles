52473281b7003aac40ecf8bdd982c2175b7817da
thistle <bcmertz@email.wm.edu>
2020-02-15 18:24:49 -0500

    js mode

--- .emacs.d/languages/custom-js.el
+++ .emacs.d/languages/custom-js.el
@@ -1,3 +1,12 @@
 ;;;;;;;;;;;;;;;;;;;;;JS Configuration;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(use-package js
+  :defer t
+  :config
+  (require 'yasnippet)
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/js-mode")
+  (yas-global-mode 1))
+
 (setq js-indent-level 2)
+
+
