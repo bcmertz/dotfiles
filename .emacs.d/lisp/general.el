9a78eaf21f45b0c658e9af4c14bb99c9527c1db6
thistle <bcmertz@email.wm.edu>
2020-02-16 15:38:03 -0500

    dont yank delete word backwards

--- .emacs.d/lisp/general.el
+++ .emacs.d/lisp/general.el
@@ -146,3 +146,20 @@
 
 ;; Git integration
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; delete don't kill backwards for M-del
+(defun delete-word (arg)
+    "Delete characters forward until encountering the end of a word.
+With argument, do this that many times."
+    (interactive "p")
+    (if (use-region-p)
+	(delete-region (region-beginning) (region-end))
+      (delete-region (point) (progn (forward-word arg) (point)))))
+
+(defun backward-delete-word (arg)
+    "Delete characters backward until encountering the end of a word.
+With argument, do this that many times."
+    (interactive "p")
+    (delete-word (- arg)))
+
+(global-set-key (read-kbd-macro "<M-DEL>") 'backward-delete-word)
