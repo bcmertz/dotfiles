28efbea492910972f4c4f463802bd3322c9a5ecd
thistle <bcmertz@email.wm.edu>
2020-02-17 00:51:36 -0500

    custom

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,11 +37,9 @@
 
 ;; Return to normal gc value
 (setq gc-cons-threshold 800000)
+
+;; Custom
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
