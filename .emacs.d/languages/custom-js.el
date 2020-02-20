8ee557cf997e41d8ad4c43ab0b39b9a7616bcf66
thistle <bcmertz@email.wm.edu>
2020-02-20 14:57:29 -0500

    remap mode-map keys

--- .emacs.d/languages/custom-js.el
+++ .emacs.d/languages/custom-js.el
@@ -3,10 +3,10 @@
 (use-package js
   :defer t
   :config
+  (define-key js-mode-map (kbd "M-.") nil)
+  (define-key js-mode-map (kbd "C-c C-f") 'js-find-symbol)
   (require 'yasnippet)
   (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/js-mode")
   (yas-global-mode 1))
 
 (setq js-indent-level 2)
-
-
