fc64d0e5b10968440bbcb795adaf4b0a7d441546
thistle <bcmertz@email.wm.edu>
2020-02-23 23:58:47 -0500

    markdown editing support

--- .emacs.d/languages/custom-markdown.el
+++ .emacs.d/languages/custom-markdown.el
@@ -1,13 +1,17 @@
 ;;;;;;;;;;;;;;;;;;;;;;; Markdown Config ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package markdown-mode
+  :mode "\\.md\\'"
+  :defer t
+  :config
+  (custom-set-variables
+  '(markdown-command "/usr/bin/pandoc"))
+  )
 
-(use-package flymd
-  :ensure t
-  :bind ("C-c m" . flymd-flyit)) ; previews
+;; :bind ("C-c m" . flymd-flyit)) ; previews
+;; (setq flymd-output-directory "/tmp")
 
-(setq flymd-output-directory "/tmp")
-
-;; Firefox for compatability
-(defun my-flymd-browser-function (url)
-  (let ((browse-url-browser-function 'browse-url-firefox))
-    (browse-url url)))
-(setq flymd-browser-open-function 'my-flymd-browser-function)
+;; ;; Firefox for compatability
+;; (defun my-flymd-browser-function (url)
+;;   (let ((browse-url-browser-function 'browse-url-firefox))
+;;     (browse-url url)))
+;; (setq flymd-browser-open-function 'my-flymd-browser-function)
