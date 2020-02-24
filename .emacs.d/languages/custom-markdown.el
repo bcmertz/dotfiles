af5e9124ced19f2e2aca103f52b140ee354f112f
thistle <bcmertz@email.wm.edu>
2020-02-23 23:59:41 -0500

    remove unused

--- .emacs.d/languages/custom-markdown.el
+++ .emacs.d/languages/custom-markdown.el
@@ -6,12 +6,3 @@
   (custom-set-variables
   '(markdown-command "/usr/bin/pandoc"))
   )
-
-;; :bind ("C-c m" . flymd-flyit)) ; previews
-;; (setq flymd-output-directory "/tmp")
-
-;; ;; Firefox for compatability
-;; (defun my-flymd-browser-function (url)
-;;   (let ((browse-url-browser-function 'browse-url-firefox))
-;;     (browse-url url)))
-;; (setq flymd-browser-open-function 'my-flymd-browser-function)
