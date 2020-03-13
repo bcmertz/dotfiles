b6d33f32a3b420bb90d409f8d3c38cfafc5dc320
thistle <bcmertz@email.wm.edu>
2020-03-13 00:33:52 -0400

    customize file location

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,7 +6,6 @@
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "lisp/languages" user-emacs-directory))
-
 (load "custom-general.el")
 (load "custom-funcs.el")
 (load "custom-styling.el")
@@ -18,21 +17,11 @@
 (load "custom-markdown.el")
 (load "custom-css.el")
 
+;; set customize config file location
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
 ;; Return to normal gc value
 (setq gc-cons-threshold 800000)
 ;; garbage collect when emacs leaves focus
 (add-hook 'focus-out-hook 'garbage-collect)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(gofmt-command "goimports" t)
- '(package-selected-packages
-   '(general yasnippet which-key vimish-fold use-package sublimity spaceline smex smartparens projectile neotree multiple-cursors markdown-mode magit go-eldoc flycheck expand-region evil counsel company-go cask avy-zap atom-one-dark-theme all-the-icons)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
