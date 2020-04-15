241b985f6cec94a45722b761b8a37e122f1023d6
thistle <bcmertz@email.wm.edu>
2020-04-15 16:47:42 -0400

    move file func

--- .emacs.d/lisp/custom-funcs.el
+++ .emacs.d/lisp/custom-funcs.el
@@ -92,4 +92,25 @@ With argument, do this that many times."
     (interactive "p")
     (delete-word (- arg)))
 
+(defun move-file (new-location)
+  "Write this file to NEW-LOCATION, and delete the old one."
+  (interactive (list (expand-file-name
+                      (if buffer-file-name
+                          (read-file-name "Move file to: ")
+                        (read-file-name "Move file to: "
+                                        default-directory
+                                        (expand-file-name (file-name-nondirectory (buffer-name))
+                                                          default-directory))))))
+  (when (file-exists-p new-location)
+    (delete-file new-location))
+  (let ((old-location (expand-file-name (buffer-file-name))))
+    (message "old file is %s and new file is %s"
+             old-location
+             new-location)
+    (write-file new-location t)
+    (when (and old-location
+               (file-exists-p new-location)
+               (not (string-equal old-location new-location)))
+            (delete-file old-location))))
+
 ;;; custom-funcs.el ends here
