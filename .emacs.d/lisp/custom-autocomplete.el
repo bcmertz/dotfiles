2f8b75d671ef4f020ad8ce534ea794f0f125323e
thistle <bcmertz@email.wm.edu>
2020-07-28 15:47:07 -0400

    autocomplete

--- .emacs.d/lisp/custom-autocomplete.el
+++ .emacs.d/lisp/custom-autocomplete.el
@@ -10,10 +10,10 @@
    :init
    (progn
      (setq company-tooltip-limit 20)                       ; bigger popup window
-     (setq company-idle-delay nil)                         ; don't autocomplete on typing, backtab instead
-     ;; setq company-idle-delay .3)  			   ; decrease delay before autocompletion popup shows
-     (setq company-echo-delay 0)                           ; remove annoying blinking
-     (setq company-begin-commands '(self-insert-command))  ; start autocompletion after typing, if we want to ignore our special tab key we bind below
+     ;; (setq company-idle-delay nil)                         ; don't autocomplete on typing, backtab instead
+     (setq company-idle-delay .3)  			      ; decrease delay before autocompletion popup shows
+     ;;(setq company-echo-delay 0)                           ; remove annoying blinking
+     ;;(setq company-begin-commands '(self-insert-command))  ; start autocompletion after typing, if we want to ignore our special tab key we bind below
      ))
 
 (global-company-mode)
