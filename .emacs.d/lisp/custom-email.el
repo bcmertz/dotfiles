ab2a29979ebd7acaadd0c3d290f2aedfd70fb53a
thistle <bcmertz@email.wm.edu>
2020-07-29 17:06:39 -0400

    attempt at email

---
+++ .emacs.d/lisp/custom-email.el
@@ -0,0 +1,12 @@
+;;; custom-email.el --- custom email
+;;;
+;;; Commentary:
+;;;
+;;; email support
+;;;
+;;; Code:
+
+(setq load-path (append load-path '("~/.emacs.d/mu/mu4e")))
+(require 'mu4e)
+
+;;; custom-email.el ends here
