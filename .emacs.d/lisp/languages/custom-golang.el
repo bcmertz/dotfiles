f36aab4472c5c79e3484b12a256c5e1de7eaebfd
thistle <bcmertz@email.wm.edu>
2020-08-11 23:39:16 -0400

    keybindings

--- .emacs.d/lisp/languages/custom-golang.el
+++ .emacs.d/lisp/languages/custom-golang.el
@@ -21,6 +21,11 @@
 (use-package go-mode
   :defer t
   :mode "\\.go\\'"
+  :config
+  (local-set-key (kbd "C-c C-r") 'go-rename)
+  (local-set-key (kbd "C-c C-p") 'godoc-at-point)
+  (local-set-key (kbd "C-c C-d") 'godef-describe)
+  (local-set-key (kbd "C-c C-t") 'gocode-toggle)
   :general
   (tyrant-def go-mode-map
     "mr"  'go-rename
@@ -28,40 +33,4 @@
     "md" 'godef-describe
     "mt"  'gocode-toggle))
 
-;; (defun go-mode-setup () ; use C-c C-j to jump to definition and C-u C-x C-x to jump back add imports as needed
-;;   (add-hook 'before-save-hook 'gofmt-before-save)
-;;   (require 'go-eldoc)
-;;   (go-eldoc-setup)                                                ; provides type info
-;;   (set-face-attribute 'eldoc-highlight-function-argument nil
-;; 		      :foreground "green"
-;; 		      :weight 'bold)
-;;   (require 'yasnippet)
-;;   (add-to-list 'load-path "~/.emacs.d/elpa/yasnippet-*")
-;;   (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/go-mode")   ; enable snippets
-;;   (yas-global-mode 1)
-;;   ;; go mode autocomplete
-;;   (require 'company-go)
-;;   (customize-set-variable 'company-go-gocode-command "gocode-mod") ; defualt to module support
-;;   (set (make-local-variable 'company-backends) '(company-go))
-;;   ;; go mode keybindings
-;;   (local-set-key (kbd "C-c C-r") 'go-rename)
-;;   (local-set-key (kbd "C-c C-p") 'godoc-at-point)
-;;   (local-set-key (kbd "C-c C-d") 'godef-describe)
-;;   (local-set-key (kbd "C-c C-t") 'gocode-toggle)
-;;   )
-
-
-;; (defun gocode-toggle ()
-;;   "Toggle the gocode executable between the module and non-module versions."
-;;   (interactive)
-;;   (customize-set-variable
-;;    'company-go-gocode-command
-;;    (if (string= company-go-gocode-command "gocode-mod")
-;;        "gocode" "gocode-mod"))
-;;   ;; The gocode fork that works with modules is slow, so disable idle completion.
-;;   ;; (if (string= company-go-gocode-command "gocode-mod")
-;;   ;;     (customize-set-variable 'company-idle-delay nil)
-;;   ;;   (custom-reevaluate-setting 'company-idle-delay))
-;;   (message company-go-gocode-command))
-
-;; ;;; custom-golang.el ends here
+;;; custom-golang.el ends here
