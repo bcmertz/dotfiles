c9ce8e15a1308afe9f9f12ca6419dfb1b0d9ded1
thistle <bcmertz@email.wm.edu>
2020-10-06 23:20:15 -0400

    typescript indent level

--- .emacs.d/lisp/languages/custom-typescript.el
+++ .emacs.d/lisp/languages/custom-typescript.el
@@ -8,6 +8,10 @@
 
 (use-package typescript-mode
   :defer t
+  :init
+  (setq typescript-indent-level 2)
   )
+
+;; (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
 
 ;;; custom-typescript.el ends here
