1c58dc385beb0cc327a591b4a8936c86436749e9
thistle <bcmertz@email.wm.edu>
2020-10-17 14:25:03 -0400

    typescript checking

--- .emacs.d/lisp/custom-flycheck.el
+++ .emacs.d/lisp/custom-flycheck.el
@@ -16,6 +16,8 @@
       (flycheck-select-checker 'javascript-eslint))
     (add-hook 'js-mode-hook #'setup-js-mode))
   :config
+  (flycheck-add-mode 'typescript-tslint 'web-mode)
+
   (setq-default flycheck-check-syntax-automatically '(save))
   ;; disable documentation related emacs lisp checker
   (setq-default flycheck-disabled-checkers '(go-staticcheck))
