5509d0a43cad3d96b4ff83a25d4d81ea07255f5f
thistle <bcmertz@email.wm.edu>
2020-10-17 14:26:02 -0400

    web mode handles tsx jsx html files

--- .emacs.d/lisp/languages/custom-typescript.el
+++ .emacs.d/lisp/languages/custom-typescript.el
@@ -2,16 +2,33 @@
 ;;;
 ;;; Commentary:
 ;;;
-;;;
-;;;
 ;;; Code:
 
-(use-package typescript-mode
+;; (use-package typescript-mode
+;;   :defer t
+;;   :init
+;;   (setq typescript-indent-level 2)
+;;   )
+
+(use-package web-mode
   :defer t
-  :init
-  (setq typescript-indent-level 2)
+  :mode (
+         ("\\.[jt]sx\\'" . web-mode)
+	 ("\\.html\\'" . web-mode)
+         )
+  :after flycheck
   )
 
-;; (add-to-list 'auto-mode-alist '("\\.tsx\\'" . typescript-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+    (setq web-mode-markup-indent-offset 2)
+    (setq web-mode-css-indent-offset 2)
+    (setq web-mode-code-indent-offset 2)
+    (setq web-mode-indent-style 2)
+    ;;(flycheck-select-checker 'typescript-tslint)
+    ;;(flycheck-add-mode 'typescript-tslint 'web-mode)
+    )
+
+(add-hook 'web-mode-hook  'my-web-mode-hook)
 
 ;;; custom-typescript.el ends here
