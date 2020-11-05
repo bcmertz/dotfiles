32e57a7dd98041be957baf6ab3d7ffb6108e2c87
thistle <bcmertz@email.wm.edu>
2020-11-05 13:55:31 -0500

    defer webmode

--- .emacs.d/lisp/languages/custom-html.el
+++ .emacs.d/lisp/languages/custom-html.el
@@ -7,16 +7,17 @@
 ;;; Code:
 
 (use-package web-mode
-:ensure t
-:config
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(setq web-mode-engines-alist
-'(("django"    . "\\.html\\'")))
-(setq web-mode-ac-sources-alist
-'(("css" . (ac-source-css-property))
-("html" . (ac-source-words-in-buffer ac-source-abbrev))))
+  :defer t
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (setq web-mode-engines-alist
+        '(("django"    . "\\.html\\'")))
+  (setq web-mode-ac-sources-alist
+        '(("css" . (ac-source-css-property))
+          ("html" . (ac-source-words-in-buffer ac-source-abbrev))))
 
-(setq web-mode-enable-auto-closing t)
-(setq web-mode-enable-auto-quoting t)) ; this fixes the quote problem I mentioned
+  (setq web-mode-enable-auto-closing t)
+  (setq web-mode-enable-auto-quoting t)) ; this fixes the quote problem I mentioned
 
 ;;; custom-html.el ends here
