4d43388c1b6a124b4b050c5c3ac3ab5073590b07
thistle <bcmertz@email.wm.edu>
2020-12-15 17:04:40 -0500

    s

--- .emacs.d/lisp/custom-navigation.el
+++ .emacs.d/lisp/custom-navigation.el
@@ -25,7 +25,7 @@
 (projectile-mode +1)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
-(setq projectile-project-search-path '("~/coding/" "~/go/src/github.com/getlantern/"))
+(setq projectile-project-search-path '("~/coding/"))
 
 
 ;; perspective navigation
