1ca85a052848f06430684c8a6ccf661048276258
thistle <bcmertz@email.wm.edu>
2020-12-31 16:33:27 -0500

    s

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(desktop-environment exwm pass magit perspective flycheck yasnippet which-key web-mode vimish-fold use-package sublimity smex smartparens projectile pdf-tools org-bullets neotree multiple-cursors markdown-mode general evil eglot doom-modeline counsel company avy auctex atom-one-dark-theme)))
+   '(go-mode desktop-environment exwm pass magit perspective flycheck yasnippet which-key web-mode vimish-fold use-package sublimity smex smartparens projectile pdf-tools org-bullets neotree multiple-cursors markdown-mode general evil eglot doom-modeline counsel company avy auctex atom-one-dark-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
