26d8614ef5f764893086952f70dbbad822d6419d
thistle <bcmertz@email.wm.edu>
2021-01-23 18:26:55 -0500

    mail

--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -6,7 +6,16 @@
 ;;;
 ;;; Code:
 
-(setq load-path (append load-path '("~/.emacs.d/mu/mu4e")))
+(load-file "~/.config/mu4e/mu4e-config.el")
+
+(add-to-list 'load-path "~/.config/mu4e")
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 (require 'mu4e)
+(require 'mu4e-config)
+
+(use-package mu4e-config
+  :after mu4e
+  :load-path "~/.config/mu4e")
+
 
 ;;; custom-email.el ends here
