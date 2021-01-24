c2efe9aad71efe880e2b8e742011de44266d50c0
thistle <bcmertz@email.wm.edu>
2021-01-24 14:26:18 -0500

    config

--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -3,21 +3,18 @@
 ;;; Commentary:
 ;;;
 ;;; email support
+;;; configured with https://github.com/cemkeylan/mu-wizard/
+;;; rest of config is in ~/.config/mu4e and ~/.config/msmtp
+;;; mu-wizard handles ~/.config config
 ;;;
 ;;; Code:
-
-(add-to-list 'load-path "~/.config/mu4e")
-(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
 
 (use-package mu4e
   :load-path "~/.emacs.d/elpa/mu4e"
   )
 
-(require 'mu4e-config)
-
 (use-package mu4e-config
   :after mu4e
   :load-path "~/.config/mu4e")
-
 
 ;;; custom-email.el ends here
