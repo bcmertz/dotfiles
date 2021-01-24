41456cfcf412044dab54addf58fa505242eb93ce
thistle <bcmertz@email.wm.edu>
2021-01-23 20:44:32 -0500

    mu4e

--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -8,8 +8,9 @@
 
 (add-to-list 'load-path "~/.config/mu4e")
 (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
 (use-package mu4e
-  :load-path "/usr/share/emacs/site-lisp/mu4e"
+  :load-path "~/.emacs.d/elpa/mu4e"
   )
 
 (require 'mu4e-config)
