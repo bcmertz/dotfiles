30304b71408b760952976f241ee62a5ce16cc9ac
thistle <bcmertz@email.wm.edu>
2021-01-28 16:00:47 -0500

    mark whole word

--- .emacs.d/lisp/custom-funcs.el
+++ .emacs.d/lisp/custom-funcs.el
@@ -201,5 +201,35 @@ With argument, do this that many times."
   (org-cycle)
   )
 
+;; https://emacs.stackexchange.com/a/35072
+;; mark whole word
+(defun mark-whole-word (&optional arg allow-extend)
+  "Like `mark-word', but selects whole words and skips over whitespace.
+If you use a negative prefix arg then select words backward.
+Otherwise select them forward.
+
+If cursor starts in the middle of word then select that whole word.
+
+If there is whitespace between the initial cursor position and the
+first word (in the selection direction), it is skipped (not selected).
+
+If the command is repeated or the mark is active, select the next NUM
+words, where NUM is the numeric prefix argument.  (Negative NUM
+selects backward.)"
+  (interactive "P\np")
+  (let ((num  (prefix-numeric-value arg)))
+    (unless (eq last-command this-command)
+      (if (natnump num)
+          (skip-syntax-forward "\\s-")
+        (skip-syntax-backward "\\s-")))
+    (unless (or (eq last-command this-command)
+                (if (natnump num)
+                    (looking-at "\\b")
+                  (looking-back "\\b")))
+      (if (natnump num)
+          (left-word)
+        (right-word)))
+    (mark-word arg allow-extend)))
+
 
 ;;; custom-funcs.el ends here
