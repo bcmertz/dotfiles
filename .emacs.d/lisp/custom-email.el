97fd87f08c93ae3319e8b24e26c452d8636d1c9d
thistle <bcmertz@email.wm.edu>
2021-02-07 13:56:56 -0500

    remove old command

--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -29,8 +29,6 @@
   (setq mu4e-change-filenames-when-moving t)
   ;; update script every two minutes
   (setq mu4e-update-interval 120
-        mu4e-headers-auto-update t)
-        ;; mu4e-get-mail-command "~/.local/bin/mbsync/sort_and_fetch"
-  )
+        mu4e-headers-auto-update t))
 
 ;;; custom-email.el ends here
