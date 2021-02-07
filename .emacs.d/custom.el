f4a90f543cf256758375264a3def3b855760f301
thistle <bcmertz@email.wm.edu>
2021-02-07 13:23:28 -0500

    mu4e alerts

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,8 +3,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-confirm-babel-evaluate nil)
+ '(org-export-html-postamble nil)
+ '(org-export-with-toc nil)
+ '(org-hide-leading-stars t)
+ '(org-src-fontify-natively t)
+ '(org-startup-folded 'overview)
+ '(org-startup-indented t)
  '(package-selected-packages
-   '(htmlize ox-reveal buffer-move go-mode desktop-environment exwm pass magit perspective flycheck yasnippet which-key web-mode vimish-fold use-package sublimity smex smartparens projectile pdf-tools org-bullets neotree multiple-cursors markdown-mode general evil eglot doom-modeline counsel company avy auctex atom-one-dark-theme)))
+   '(mu4e-alert htmlize ox-reveal buffer-move go-mode desktop-environment exwm pass magit perspective flycheck yasnippet which-key web-mode vimish-fold use-package sublimity smex smartparens projectile pdf-tools org-bullets neotree multiple-cursors markdown-mode general evil eglot doom-modeline counsel company avy auctex atom-one-dark-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -17,4 +17,21 @@
   :after mu4e
   :load-path "~/.config/mu4e")
 
+(use-package mu4e-alert
+  :ensure t
+  :after mu4e
+  :init
+  (mu4e-alert-set-default-style 'libnotify)
+  (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+  (setq mu4e-alert-email-notification-types '(count))
+  (mu4e-alert-enable-mode-line-display)
+  ;; delete it when told
+  (setq mu4e-change-filenames-when-moving t)
+  ;; update script every two minutes
+  (setq mu4e-get-mail-command "~/.local/bin/mbsync/sort_and_fetch"
+      mu4e-update-interval 120
+      mu4e-headers-auto-update t)
+  )
+
 ;;; custom-email.el ends here
