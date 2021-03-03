1f1e4dffef2c280aa8282fd46c3017189cf3e594
thistle <bcmertz@email.wm.edu>
2021-03-03 15:24:23 -0500

    check if term or gui

--- .emacs.d/lisp/custom-email.el
+++ .emacs.d/lisp/custom-email.el
@@ -9,26 +9,31 @@
 ;;;
 ;;; Code:
 
-(use-package mu4e
-  :load-path "~/.emacs.d/elpa/mu4e" ;; requires moving /usr/share/emacs/site-lisp/mu4e
+;; dont start email services for terminal daemon :)
+(if (display-graphic-p)
+    (progn
+      (use-package mu4e
+        :load-path "~/.emacs.d/elpa/mu4e" ;; requires moving /usr/share/emacs/site-lisp/mu4e
+        )
+
+      (use-package mu4e-config
+        :after mu4e
+        :load-path "~/.config/mu4e")
+
+      (use-package mu4e-alert
+        :ensure t
+        :after mu4e
+        :init
+        (mu4e-alert-set-default-style 'libnotify)
+        (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
+        (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
+        (setq mu4e-alert-email-notification-types '(count))
+        (mu4e-alert-enable-mode-line-display)
+        ;; (setq mu4e-change-filenames-when-moving t)
+        ;; update script every two minutes
+        (setq mu4e-update-interval 300
+              mu4e-headers-auto-update t))
+      )
   )
-
-(use-package mu4e-config
-  :after mu4e
-  :load-path "~/.config/mu4e")
-
-(use-package mu4e-alert
-  :ensure t
-  :after mu4e
-  :init
-  (mu4e-alert-set-default-style 'libnotify)
-  (add-hook 'after-init-hook #'mu4e-alert-enable-notifications)
-  (add-hook 'after-init-hook #'mu4e-alert-enable-mode-line-display)
-  (setq mu4e-alert-email-notification-types '(count))
-  (mu4e-alert-enable-mode-line-display)
-  ;; (setq mu4e-change-filenames-when-moving t)
-  ;; update script every two minutes
-  (setq mu4e-update-interval 300
-        mu4e-headers-auto-update t))
 
 ;;; custom-email.el ends here
