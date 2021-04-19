c0759feebc9764f51d501922c2684e1026408682
thistle <bcmertz@email.wm.edu>
2021-04-19 17:39:16 -0400

    pdf-tools

--- .emacs.d/lisp/custom-pdf.el
+++ .emacs.d/lisp/custom-pdf.el
@@ -9,15 +9,23 @@
   :mode ("\\.pdf\\'" . pdf-view-mode)
   :ensure t
   :defer t
-  ;; :bind TODO
+  :bind
   ;; ("<left>" . image-backward-hscroll)
   ;; ("<right>" . image-forward-hscroll)
-  ;; ("C-s" . pdf-occur)
+  ("C-s" . pdf-occur)
   :config
   (setq-default pdf-view-display-size 'fit-page)
   (setq pdf-annot-activate-created-annotations t)
-  ;; (pdf-tools-install :no-query) TODO
-  ;; (require 'pdf-occur)
+  (doc-view-mode)
+  (pdf-tools-install :no-query)
+  (require 'pdf-occur)
+  ;; keyboard shortcuts
+  (define-key pdf-view-mode-map (kbd "C-s") 'pdf-occur)
+  (define-key pdf-view-mode-map (kbd "h") 'pdf-annot-add-highlight-markup-annotation)
+  (define-key pdf-view-mode-map (kbd "t") 'pdf-annot-add-text-annotation)
+  (define-key pdf-view-mode-map (kbd "D") 'pdf-annot-delete)
+  (define-key pdf-view-mode-map (kbd "A") 'pdf-annot-list-annotations)
   )
+
 
 ;;; custom-pdf.el ends here
