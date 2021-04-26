1c38a69be19ea285cd51fa004212a777bca0276b
thistle <bcmertz@email.wm.edu>
2021-04-25 21:06:06 -0400

    compile in comint mode by default

--- .emacs.d/lisp/custom-compile.el
+++ .emacs.d/lisp/custom-compile.el
@@ -6,11 +6,18 @@
 ;;;
 ;;; Code:
 
+(defun compile-comint ()
+  "Compile but force interactive comint mode."
+  (interactive)
+  (setq current-prefix-arg '(4))
+  (call-interactively 'compile)
+  )
+
 (use-package compile
   :config
   (setq compilation-scroll-output 1)
   :bind
-  ("C-c c" . compile)
+  ("C-c c" . compile-comint)
   )
 
 ;;; custom-compile.el ends here
