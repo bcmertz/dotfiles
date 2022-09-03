63bb95f282d661d0dd51e5465b2d7926f87194b5
leah <bennett.mertz@gmail.com>
2022-09-02 21:04:51 -0400

    pinentry

--- .emacs.d/lisp/custom-pass.el
+++ .emacs.d/lisp/custom-pass.el
@@ -12,4 +12,12 @@
   (setq pass-username-fallback-on-filename t)
   )
 
+;; ;; pinentry for emacs
+;; (use-package pinentry
+;;   :config
+;;   (setq epa-pinentry-mode 'loopback)
+;;   :init
+;;   (pinentry-start)
+;;   :ensure t)
+
 ;;; custom-pass.el ends here
