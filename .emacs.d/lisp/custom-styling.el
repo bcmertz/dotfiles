2737b33196c2727647ac0e06020e2bdec42e3fbf
leah <bennett.mertz@gmail.com>
2022-09-03 21:27:40 -0400

    variable pitch height

--- .emacs.d/lisp/custom-styling.el
+++ .emacs.d/lisp/custom-styling.el
@@ -147,7 +147,7 @@
   (set-fontset-font t 'symbol "UbuntuMono Nerd Font" nil 'prepend)
   (set-face-attribute 'default nil :font "Monospace" :weight 'light :height 110)
   (set-face-attribute 'fixed-pitch nil :font "Monospace" :weight 'light :height 110)
-  (set-face-attribute 'variable-pitch nil :font "ETBembo" :weight 'thin)
+  (set-face-attribute 'variable-pitch nil :font "ETBembo" :weight 'thin :height 125)
   )
 
 ;; respect default terminal fonts
