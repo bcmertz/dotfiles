a704aba9c61f2482cc76b9f9d203e17a08a7a488
leah <bennett.mertz@gmail.com>
2022-09-09 21:05:06 -0400

    doc

--- .emacs.d/lisp/custom-git.el
+++ .emacs.d/lisp/custom-git.el
@@ -15,7 +15,8 @@
 
 (defun ~/magit-process-environment (env)
   "Add GIT_DIR and GIT_WORK_TREE to ENV when in a special directory.
-https://github.com/magit/magit/issues/460 (@cpitclaudel)."
+https://github.com/magit/magit/issues/460 (@cpitclaudel) and
+https://github.com/magit/magit/issues/460#issuecomment-837449105."
   (let ((default (file-name-as-directory (expand-file-name default-directory)))
         (home (expand-file-name "~/")))
     (when (string= default home)
