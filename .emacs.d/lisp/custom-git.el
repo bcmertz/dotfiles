1bf5254087aeed62f72d84b1bc3ababc3ecd0a30
leah <bennett.mertz@gmail.com>
2022-09-09 21:01:59 -0400

    magit find GIT_DIR in home directory as .cfg

--- .emacs.d/lisp/custom-git.el
+++ .emacs.d/lisp/custom-git.el
@@ -13,6 +13,20 @@
   ("C-x c" . magit-checkout)
   ("C-x l" . magit-log-branches))
 
+(defun ~/magit-process-environment (env)
+  "Add GIT_DIR and GIT_WORK_TREE to ENV when in a special directory.
+https://github.com/magit/magit/issues/460 (@cpitclaudel)."
+  (let ((default (file-name-as-directory (expand-file-name default-directory)))
+        (home (expand-file-name "~/")))
+    (when (string= default home)
+      (let ((gitdir (expand-file-name "~/.cfg/")))
+        (push (format "GIT_WORK_TREE=%s" home) env)
+        (push (format "GIT_DIR=%s" gitdir) env))))
+  env)
+
+(advice-add 'magit-process-environment
+            :filter-return #'~/magit-process-environment)
+
 (use-package github-review
   :defer t
   :after magit
