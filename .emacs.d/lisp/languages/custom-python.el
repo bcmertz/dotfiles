50393cea52662efcfd4baa9e29c2a5813529cbf4
leah <bennett.mertz@gmail.com>
2022-11-28 02:32:22 -0500

    rm

--- .emacs.d/lisp/languages/custom-python.el
+++ .emacs.d/lisp/languages/custom-python.el
@@ -9,10 +9,10 @@
 (with-eval-after-load 'python
   (define-key python-mode-map (kbd "<backtab>") #'company-complete))
 
-(use-package lsp-pyright
-  :ensure t
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
+;; (use-package lsp-pyright
+;;   :ensure t
+;;   :hook (python-mode . (lambda ()
+;;                           (require 'lsp-pyright)
+;;                           (lsp))))  ; or lsp-deferred
 
 ;;; custom-python.el ends here
