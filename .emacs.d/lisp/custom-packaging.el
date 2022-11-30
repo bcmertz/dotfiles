cb5fe42a8adc8d30a07ed9730670b6b5e56806bb
leah <bennett.mertz@gmail.com>
2022-11-30 17:07:59 -0500

    nongnu elpa repo

--- .emacs.d/lisp/custom-packaging.el
+++ .emacs.d/lisp/custom-packaging.el
@@ -12,8 +12,8 @@
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
+                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
-
 ;; unless we have use-package, refresh cache and install selected
 (unless (package-installed-p 'use-package)
   (progn
