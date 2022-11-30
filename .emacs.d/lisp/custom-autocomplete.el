4159e3fcf6310c15e15ed69307ebddf83156064b
leah <bennett.mertz@gmail.com>
2022-11-30 17:09:10 -0500

    yasnippet prefix, orderless + corfu, remove company

--- .emacs.d/lisp/custom-autocomplete.el
+++ .emacs.d/lisp/custom-autocomplete.el
@@ -8,31 +8,119 @@
 ;;; Code:
 
 (setq completion-ignore-case t)
+(setq tab-always-indent t)
+(global-set-key [backtab] 'completion-at-point) ;; backtab triggers autocomplete
 
 
-(use-package company
-  :defer t
-  :after eldoc
-  :init
-  (progn
-    (global-company-mode)
-    (setq company-tooltip-limit 20)                         ; bigger popup window
-    (setq company-idle-delay nil)                           ; don't autocomplete on typing, backtab instead
-    ;; (setq company-idle-delay .3)  			     ; decrease delay before autocompletion popup shows
-    ;;(setq company-echo-delay 0)                           ; remove annoying blinking
-    ;;(setq company-begin-commands '(self-insert-command))  ; start autocompletion after typing, if we want to ignore our special tab key we bind below
-    ))
-
-
-(use-package company-box
+(use-package corfu
   :ensure t
+  :after eldoc
+  :config
+  (setq corfu-separator ?\s)             ;; Orderless field separator - space
+  (setq corfu-popupinfo-delay 0.2)
+  :bind
+  ;; Configure SPC for separator insertion, plays nicely with orderless for completion
+  (:map corfu-map ("SPC" . corfu-insert-separator))
+  :init
+  (global-corfu-mode)
+  (corfu-popupinfo-mode)
+  (corfu-history-mode)
+  (eldoc-add-command #'corfu-insert)
+  )
+
+
+
+
+;;;;;;;;;;;; builtin arglist and variable docstrings info in echo area ;;;;;;;;;;;;;;
+
+(use-package eldoc
+  :config
+  (setq eldoc-idle-delay 0.2)
+  :init
+  (global-eldoc-mode)
+  )
+
+
+;;;;;;;;;;;;;;;;;;;; Completion styling ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; orderless completion style - allow space separated `search terms'
+(use-package orderless
+  :ensure t
+  :init
+  ;; Configure a custom style dispatcher (see the Consult wiki)
+  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
+  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
+  (setq completion-styles '(orderless)
+        completion-category-defaults nil
+        completion-category-overrides '((file (styles . (partial-completion))))))
+
+
+
+;;;;;;;;;;;;;;;;;;;;; syntax highlighting ;;;;;;;;;;;;;;;;;;;;;
+
+(use-package tree-sitter
+  :ensure t
+  :config
+  ;; activate tree-sitter on any buffer containing code for which it has a parser available
+  (global-tree-sitter-mode)
+  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
+  ;; by switching on and off
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :ensure t)
+
+
+;;;;;;;;;;;;;;; snippets ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-package yasnippet
   :defer t
-  :hook
-  (company-mode . company-box-mode))
+  :ensure t
+  :bind
+  (:map yas-minor-mode-map
+        ("C-c &" . nil)
+        ("C-c y i" . yas-insert-snippet)
+        ("C-c y n" . yas-new-snippet)
+        ("C-c y v" . yas-visit-snippet-file))
+  :config
+  (which-key-add-key-based-replacements "C-c y" "yasnippet")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/sh-mode")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/js-mode")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/go-mode")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/web-mode")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/org-mode")
+  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/emacs-lisp-mode")
+  (yas-global-mode 1)
+  )
 
-;; (global-set-key [tab] 'tab-indent-or-complete)
-(global-set-key [backtab] 'company-complete-common) ;; backtab triggers autocomplete
 
+
+;;;;;;;;;;;;;;;;;;;;; DEPRECATED - replaced by corfu
+
+;; (use-package company
+;;   :defer t
+;;   :after eldoc
+;;   :init
+;;   (progn
+;;     (global-company-mode)
+;;     (setq company-tooltip-limit 20)                         ; bigger popup window
+;;     (setq company-idle-delay nil)                           ; don't autocomplete on typing, backtab instead
+;;     ;; (setq company-idle-delay .3)  			     ; decrease delay before autocompletion popup shows
+;;     ;;(setq company-echo-delay 0)                           ; remove annoying blinking
+;;     ;;(setq company-begin-commands '(self-insert-command))  ; start autocompletion after typing, if we want to ignore our special tab key we bind below
+;;     ))
+
+
+;; (use-package company-box
+;;   :ensure t
+;;   :defer t
+;;   :hook
+;;   (company-mode . company-box-mode))
+
+;; ;; (global-set-key [tab] 'tab-indent-or-complete)
+;; (global-set-key [backtab] 'company-complete-common) ;; backtab triggers autocomplete
+
+;; useful if using tab for expanding autocomplete and indent
 ;; https://www.emacswiki.org/emacs/CompanyMode#toc11
 ;; (defun check-expansion ()
 ;;   (save-excursion
@@ -56,21 +144,7 @@
 ;;             (company-complete-common)
 ;;           (indent-for-tab-command)))))
 
-(use-package yasnippet
-  :defer t
-  :ensure t
-  :config
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/sh-mode")
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/js-mode")
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/go-mode")
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/web-mode")
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/org-mode")
-  (add-to-list 'yas-snippet-dirs "~/.emacs.d/snippets/emacs-lisp-mode")
-  (which-key-add-key-based-replacements "C-c y" "yasnippet")
-  (global-set-key (kbd "C-c y i") 'yas-insert-snippet)
-  (global-set-key (kbd "C-c y n") 'yas-new-snippet)
-  (global-set-key (kbd "C-c y v") 'yas-visit-snippet-file)
-  (yas-global-mode 1)
-  )
+
+
 
 ;;; custom-autocomplete.el ends here
