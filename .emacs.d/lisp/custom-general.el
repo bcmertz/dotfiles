e93e9ae8ef086164c4ad8a2bf8c085ffbaad3160
reya <leah.mertz@protonmail.com>
2022-12-16 13:53:43 -0500

    path

--- .emacs.d/lisp/custom-general.el
+++ .emacs.d/lisp/custom-general.el
@@ -39,7 +39,7 @@
       ;; (make-local-variable 'kill-buffer-query-functions)
       ;; (add-hook 'kill-buffer-query-functions 'kill-scratch-buffer)
       (delete-region (point-min) (point-max))
-      (insert-image (create-image "~/.emacs.d/img/ue-colorful.png"))
+      (insert-image (create-image "~/.emacs.d/etc/ue-colorful.png"))
       (when initial-scratch-message
         (insert (substitute-command-keys initial-scratch-message))
         (set-buffer-modified-p nil))
