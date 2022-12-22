29a972621ebdad1bbd9ec35e6be2adcede48cd1a
reya <leah.mertz@protonmail.com>
2022-12-22 12:09:23 -0500

    rm unused

--- .emacs.d/lisp/custom-completion.el
+++ .emacs.d/lisp/custom-completion.el
@@ -43,9 +43,6 @@
 ;; orderless completion style - allow space separated `search terms'
 (use-package orderless
   :init
-  ;; Configure a custom style dispatcher (see the Consult wiki)
-  ;; (setq orderless-style-dispatchers '(+orderless-dispatch)
-  ;;       orderless-component-separator #'orderless-escapable-split-on-space)
   (setq completion-styles '(orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles . (partial-completion))))))
