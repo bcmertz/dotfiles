8275ee3e3259f5f93298af6c430df7545180e9cf
reya <leah.mertz@protonmail.com>
2022-12-23 12:58:14 -0500

    use straight and use-package

--- .emacs.d/lisp/custom-packaging.el
+++ .emacs.d/lisp/custom-packaging.el
@@ -6,25 +6,42 @@
 ;;;
 ;;; Code:
 
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
 ;; Packages and repository management
-(require 'package)
+;; (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+;;                          ("melpa-stable" . "https://stable.melpa.org/packages/")
+;;                          ("org" . "https://orgmode.org/elpa/")
+;;                          ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+;;                          ("elpa" . "https://elpa.gnu.org/packages/")))
 
-(setq use-package-always-ensure t)
-;; (setq use-package-verbose t)
+;; (setq use-package-always-ensure t)
+;; ;; (setq use-package-verbose t)
 
-;; unless we have use-package, refresh cache and install use-package
-(unless (package-installed-p 'use-package)
-  (progn
-    (unless package-archive-contents
-      (package-refresh-contents))
-    (package-install 'use-package)))
+;; ;; unless we have use-package, refresh cache and install use-package
+;; (unless (package-installed-p 'use-package)
+;;   (progn
+;;     (unless package-archive-contents
+;;       (package-refresh-contents))
+;;     (package-install 'use-package)))
 
-(require 'use-package)
+;; (require 'use-package)
 
 ;;; custom-packaging.el ends here
