e0d21c9df34bc1b10f898b2935d380c48a2b5283
reya <leah.mertz@protonmail.com>
2022-12-23 17:47:30 -0500

    configure tree-sitter

--- .emacs.d/lisp/custom-completion.el
+++ .emacs.d/lisp/custom-completion.el
@@ -57,16 +57,25 @@
 
 ;;;;;;;;;;;;;;;;;;;;; syntax highlighting ;;;;;;;;;;;;;;;;;;;;;
 
-(use-package tree-sitter
-  :config
-  ;; activate tree-sitter on any buffer containing code for which it has a parser available
-  (global-tree-sitter-mode)
-  ;; you can easily see the difference tree-sitter-hl-mode makes for python, ts or tsx
-  ;; by switching on and off
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(straight-use-package 'tree-sitter)
+(straight-use-package 'tree-sitter-langs)
 
-(use-package tree-sitter-langs
-  :defer t)
+;; turn on tree-sitter only for programming modes - breaks for some reason
+;; (defun turn-on-tree-sitter ()
+;;   "Turn on tree-sitter."
+;;   (tree-sitter-mode 1)
+;;   )
+;; (add-hook 'prog-mode-hook #'turn-on-tree-sitter)
+(global-tree-sitter-mode)
+
+
+
+;; always turn on tree-sitter-hl-mode
+(add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode)
+
+;; prevent bolding function calls when the cursor isn't on them
+(set-face-attribute 'tree-sitter-hl-face:function.call nil :weight 'normal)
+
 
 ;;;;;;;;;;;;;;; snippets ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
