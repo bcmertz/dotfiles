d371c562c0c4eab5e863a57b92098429b9e8cb2f
reya <leah.mertz@protonmail.com>
2023-01-03 22:51:24 -0500

    dont ensure

--- .emacs.d/lisp/custom-navigation2.el
+++ .emacs.d/lisp/custom-navigation2.el
@@ -218,7 +218,6 @@
 
 ;; Consult users will also want the embark-consult package.
 (use-package embark-consult
-  :ensure t ; only need to install it, embark loads it after consult if found
   :hook
   (embark-collect-mode . consult-preview-at-point-mode))
 
