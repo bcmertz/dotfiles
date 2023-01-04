eaeb529a8610d88224fc6c7497dbc432d3c2faf7
reya <leah.mertz@protonmail.com>
2023-01-04 18:53:23 -0500

    keybindings

--- .emacs.d/lisp/custom-general.el
+++ .emacs.d/lisp/custom-general.el
@@ -111,33 +111,4 @@
          ;;       '(file))
          )))
 
-
-;; terminal specific escape codes
-;; found with showkey -a
-(add-hook 'tty-setup-hook
-          (lambda ()
-             (define-key input-decode-map "\e[1;3A" [M-up])
-             (define-key input-decode-map "\e[1;3B" [(meta down)])
-             (define-key input-decode-map "\e[1;3C" [(meta right)])
-             (define-key input-decode-map "\e[1;3D" [(meta left)])
-             (define-key input-decode-map "\e[1;2A" [S-up])
-             (define-key input-decode-map "\e[1;2B" [(shift down)])
-             (define-key input-decode-map "\e[1;2C" [(shift right)])
-             (define-key input-decode-map "\e[1;2D" [(shift left)])
-             (define-key input-decode-map "\e[1;5A" [(C-up)])
-             (define-key input-decode-map "\e[1;5B" [(control down)])
-             (define-key input-decode-map "\e[1;5C" [(control right)])
-             (define-key input-decode-map "\e[1;5D" [(control left)])
-             (define-key input-decode-map "\e[1;7A" [(C-M-up)])
-             (define-key input-decode-map "\e[1;7B" [(C-M-down)])
-             (define-key input-decode-map "\e[1;7C" [(C-M-right)])
-             (define-key input-decode-map "\e[1;7D" [(C-M-left)])
-             ;; quick and dirty way to bind C-/ to undo in terminal
-             (global-set-key (kbd "C-_") 'undo)
-             ))
-(if (equal "st-meta-256color" (tty-type))
-    (define-key input-decode-map "\e[1;2A\" [S-up]))
-(defadvice terminal-init-xterm (after select-shift-up activate)
-  (define-key input-decode-map \"\e[1;2A\" [S-up]))]))]"))
-
 ;;; custom-general.el ends here
