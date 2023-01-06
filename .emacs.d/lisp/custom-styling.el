95c7557d4b99ae366a97b2c3e9872975b98dbdef
reya <leah.mertz@protonmail.com>
2023-01-06 16:40:00 -0500

    no custom face

--- .emacs.d/lisp/custom-styling.el
+++ .emacs.d/lisp/custom-styling.el
@@ -100,10 +100,10 @@
   (setq doom-modeline-vcs-max-length 100)
   ;; lsp
   (setq doom-modeline-lsp nil)
-  :custom-face
+  ;; :custom-face
   ;; (mode-line ((t (:foreground "#D8DEE8" :background "#353645"))))
   ;; (mode-line-inactive ((t (:background "#181A1F"))))
-  (doom-modeline-buffer-modified ((t (:inherit (error bold) :foreground "#599DD5"))))
+  ;; (doom-modeline-buffer-modified ((t (:inherit (error bold) :foreground "#599DD5"))))
   :init (doom-modeline-mode 1))
 
 (use-package hide-mode-line :defer t)
