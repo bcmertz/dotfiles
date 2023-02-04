949b6d62657f91831234e5af59f1d23ffbc45937
reya <leah.mertz@protonmail.com>
2023-02-04 00:18:55 -0500

    toggle-vterm

--- .emacs.d/lisp/custom-terminal.el
+++ .emacs.d/lisp/custom-terminal.el
@@ -8,8 +8,15 @@
 
 (use-package vterm
   :defer t
-  :bind* (("C-<return>" . vterm)
+  :bind* (("C-<return>" . toggle-vterm)
           :map vterm-mode-map (("C-c C-t" . vterm-copy-mode))))
+
+(defun toggle-vterm ()
+  "Toggle vterm"
+  (interactive)
+  (if (string= (buffer-name) "*vterm*")
+      (switch-to-prev-buffer)
+    (vterm)))
 
 (provide 'custom-terminal)
 ;;; custom-terminal.el ends here
