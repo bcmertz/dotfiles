e76b5b4e4df450d89775368aede1b376558c69ed
reya <leah.mertz@protonmail.com>
2023-02-06 18:00:27 -0500

    vterm toggle

--- .emacs.d/lisp/custom-terminal.el
+++ .emacs.d/lisp/custom-terminal.el
@@ -8,15 +8,41 @@
 
 (use-package vterm
   :defer t
-  :bind* (("C-<return>" . toggle-vterm)
-          :map vterm-mode-map (("C-c C-t" . vterm-copy-mode))))
+  :bind* (:map vterm-mode-map (("C-c C-t" . vterm-copy-mode))
+               ;; ("C-<return>" . vterm-toggle)
+               ))
 
-(defun toggle-vterm ()
-  "Toggle vterm"
-  (interactive)
-  (if (string= (buffer-name) "*vterm*")
-      (switch-to-prev-buffer)
-    (vterm)))
+;; (defvar vterm-window-configuration nil)
+;; (defun vterm-toggle ()
+;;   "Toggle vterm"
+;;   (interactive)
+;;   (if (get-buffer "*vterm*")
+;;       ;; if vterm exists
+;;       (if (get-buffer-window "*vterm*")
+;;           ;; and is visible
+;;           (progn
+;;             ;; revert window configuration back to prior to vterm
+;;             (set-window-configuration vterm-window-configuration)
+;;             (setq vterm-window-configuration nil))
+;;         ;; and isn't visible
+;;         (progn
+;;           ;; save window configuration and display vterm
+;;           (setq vterm-window-configuration (current-window-configuration))
+;;           (display-buffer "*vterm*")))
+;;     ;; if vterm doesn't exist
+;;     (progn
+;;       ;; save window configuration and start vterm
+;;       (setq vterm-window-configuration (current-window-configuration))
+;;       (display-buffer (vterm-other-window)))
+;;     )
+;;   )
+
+(add-to-list 'display-buffer-alist
+     '("\*vterm\*"
+       (display-buffer-in-side-window)
+       (window-width . 0.45)
+       (side . right)
+       (slot . 0)))
 
 (provide 'custom-terminal)
 ;;; custom-terminal.el ends here
