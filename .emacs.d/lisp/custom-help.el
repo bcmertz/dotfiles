f0c2d55ccc24fedcdff721dd25eb37bc776fb9c3
reya <leah.mertz@protonmail.com>
2023-02-06 22:01:38 -0500

    example display buffer alist

--- .emacs.d/lisp/custom-help.el
+++ .emacs.d/lisp/custom-help.el
@@ -17,5 +17,16 @@
          ("p" . help-goto-previous-page)
          ))
 
+;; this example would cause the help buffer to always
+;; be the bottom most buffer, full-width, with a height
+;; 25% of the frame
+;;
+;; (add-to-list 'display-buffer-alist
+;;      '("\*Help\*"
+;;        (display-buffer-in-side-window)
+;;        (window-height . (/ (frame-height) 0.5))
+;;        (side . bottom)
+;;        (slot . 0)))
+
 (provide 'custom-help)
 ;;; custom-help.el ends here
