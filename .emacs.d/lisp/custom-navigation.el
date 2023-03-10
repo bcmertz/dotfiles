e2f62fe5b9d4e8cc6a57e401d2a08188600576f9
reya <leah.mertz@protonmail.com>
2023-03-10 01:39:10 -0500

    remove unecessary posframe call

--- .emacs.d/lisp/custom-navigation.el
+++ .emacs.d/lisp/custom-navigation.el
@@ -42,7 +42,7 @@
         '(;; (consult-grep buffer)
           (file (lambda (_) (progn
                               (define-key vertico-map "/" #'vertico-directory-enter)
-                              (setq-local vertico-sort-override-function 'sort-directories-first))) posframe)
+                              (setq-local vertico-sort-override-function 'sort-directories-first))))
           (t posframe (lambda (_) (define-key vertico-map "/" #'self-insert-command))
              (vertico-posframe-fallback-mode . vertico-buffer-mode)))))
 
