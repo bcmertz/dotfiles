739b9683aa658dfba2e213c3670f739179512299
reya <leah.mertz@protonmail.com>
2023-03-12 12:21:51 -0400

    spacing

--- .emacs.d/lisp/custom-navigation.el
+++ .emacs.d/lisp/custom-navigation.el
@@ -175,7 +175,7 @@
   ;; preview for `consult-register', `consult-register-load',
   ;; `consult-register-store' and the Emacs built-ins.
   ;; (setq register-preview-delay 0.5
-        ;; register-preview-function #'consult-register-format)
+  ;; register-preview-function #'consult-register-format)
 
   ;; Optionally tweak the register preview window.
   ;; This adds thin lines, sorting and hides the mode line of the window.
