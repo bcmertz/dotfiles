06012018a9a04f9d8fc3f6d48a1bf6c4914d7fab
reya <leah.mertz@protonmail.com>
2023-04-17 19:57:25 -0400

    cleanup

--- .emacs.d/minimal-init.el
+++ .emacs.d/minimal-init.el
@@ -1,3 +1,11 @@
+;;; minimal-init.el --- loads minimal configuration -*- lexical-binding: t -*-
+;;;
+;;; Commentary:
+;;;
+;;; used for debugging when i break the regular config
+;;;
+;;; Code:
+
 ;; load paths
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 
@@ -5,5 +13,7 @@
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file nil t)
 
-(measure-time(require 'custom-packaging))        ;; repositories and package management
-(measure-time(require 'custom-backups))            ;; custom functions used throughout our configuration
+(require 'custom-packaging)        ;; repositories and package management
+(require 'custom-backups)          ;; custom functions used throughout our configuration
+
+;;; minimal-init.el ends here
