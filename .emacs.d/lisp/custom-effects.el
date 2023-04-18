7fd2a5957fbee965292603cf98791b2298df0909
reya <leah.mertz@protonmail.com>
2023-04-17 20:30:27 -0400

    effects

---
+++ .emacs.d/lisp/custom-effects.el
@@ -0,0 +1,13 @@
+;;; custom-effects.el --- custom effects -*- lexical-binding: t -*-
+;;;
+;;; Commentary:
+;;;
+;;; Code:
+
+;; https://github.com/alphapapa/snow.el
+;; https://github.com/elizagamedev/power-mode.el
+;; https://github.com/emacsmirror/redacted
+;; https://www.emacswiki.org/emacs/ZoneMode
+
+(provide 'custom-effects)
+;;; custom-effects.el ends here
