8fa6bf479ca00251cfc1bf20afbd56034bf46c25
reya <leah.mertz@protonmail.com>
2023-06-19 18:54:56 -0400

    kill window

--- .emacs.d/lisp/custom-help.el
+++ .emacs.d/lisp/custom-help.el
@@ -13,7 +13,8 @@
          ("C-<right>" . help-go-forward)
          ("i" . help-goto-info)
          ("n" . help-goto-next-page)
-         ("q" . kill-current-buffer)
+         ("q" . kill-buffer-and-window)
+         ;; ("q" . kill-current-buffer)
          ("I" . help-goto-lispref-info)
          ("p" . help-goto-previous-page)
          ))
@@ -31,7 +32,8 @@
           ("C-h o" . #'helpful-symbol)
           ("C-h x" . #'helpful-command)
           (:map helpful-mode-map
-                ("q" . kill-current-buffer)
+                ("q" . kill-buffer-and-window)
+                ;; ("q" . kill-current-buffer)
                 ("C-<left>" . helpful-go-back)
                 ("C-<right>" . helpful-go-forward))))
 
