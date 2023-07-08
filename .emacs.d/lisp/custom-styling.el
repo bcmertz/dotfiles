0ef3360ef8b040ef63f346e9f815a92c78e9fe93
reya <leah.mertz@protonmail.com>
2023-07-08 14:33:35 -0400

    text scaling

--- .emacs.d/lisp/custom-styling.el
+++ .emacs.d/lisp/custom-styling.el
@@ -43,14 +43,20 @@
 ;; (fringe-mode 0)
 
 ;; text sizing commands
-(if (display-graphic-p)  ;; use after init hook for it to work for some reason
-    (add-hook 'after-init-hook (lambda ()
-                                 (global-unset-key (kbd "C-_"))
-                                 (global-set-key (kbd "C-+") 'text-scale-increase)
-                                 (global-set-key (kbd "C-_") 'text-scale-decrease)
-                                 (global-set-key (kbd "C-M-+") 'global-text-scale-increase)
-                                 (global-set-key (kbd "C-M-_") 'global-text-scale-decrease)
-                                 )))
+(global-unset-key (kbd "C-_"))
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C-_") 'text-scale-decrease)
+(global-set-key (kbd "C-M-+") 'global-text-scale-increase)
+(global-set-key (kbd "C-M-_") 'global-text-scale-decrease)
+
+;; [DEPRECATED]
+;; (if-gui (add-hook 'after-init-hook (lambda ()
+;;                                      (global-unset-key (kbd "C-_"))
+;;                                      (global-set-key (kbd "C-+") 'text-scale-increase)
+;;                                      (global-set-key (kbd "C-_") 'text-scale-decrease)
+;;                                      (global-set-key (kbd "C-M-+") 'global-text-scale-increase)
+;;                                      (global-set-key (kbd "C-M-_") 'global-text-scale-decrease))))
+
 
 ;; center 1 buffer
 (use-package centered-window
