b18f0d027121c218243a79d0c56fcab36a7c5208
reya <leah.mertz@protonmail.com>
2026-07-30 11:25:14 -0400

    use config

--- .emacs.d/lisp/languages/custom-typescript.el
+++ .emacs.d/lisp/languages/custom-typescript.el
@@ -16,17 +16,11 @@
 ;;          ("\\.[jt]sx\\'" . web-mode)
 ;; 	 ("\\.html\\'" . web-mode)
 ;;          )
-;;   )
-
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-    (setq web-mode-markup-indent-offset 2)
-    (setq web-mode-css-indent-offset 2)
-    (setq web-mode-code-indent-offset 2)
-    (setq web-mode-indent-style 2)
-    )
-
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+;;   :config
+;;   (setq web-mode-markup-indent-offset 2)
+;;   (setq web-mode-css-indent-offset 2)
+;;   (setq web-mode-code-indent-offset 2)
+;;   (setq web-mode-indent-style 2))
 
 (provide 'custom-typescript)
 ;;; custom-typescript.el ends here
